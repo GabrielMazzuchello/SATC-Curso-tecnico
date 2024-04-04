27821b29f10542d02357d440b6066d4aadb50869
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2024-04-04 17:20:35 -0300

    Update TiposDados.py

--- Logica de programação/AulasPython/aula01/TiposDados.py
+++ Logica de programação/AulasPython/aula01/TiposDados.py
@@ -1,4 +1,4 @@
-valor = 1
+'''valor = 1
 nome = 'Gabriel'
 valorDecimal = 1.99
 
@@ -6,3 +6,26 @@ print('tipos de dados, inteiro: {}, caractere: {}, valordecimal: {}'.format(valo
 print(type(valor))
 print(type(valorDecimal))
 print(type(nome))
+
+valor1 = int(input('escreva um numero: '))
+valor2 = int(input('escreva outro numero: '))
+resultado = valor1 - valor2
+
+print('a subtração do valor {} e do valor {} é de {}'.format(valor1, valor2, resultado))
+'''
+
+'''nota1 = float(input('Informe a primeira nota do aluno: '))
+nota2 = float(input('Informe a segunda nota do aluno: '))
+
+print('A media do aluno é {}'.format((nota1+nota2)/2))'''
+
+valor1 = float(input('Digite um numero: '))
+valor2 = float(input('Digite outro numero: '))
+
+print('soma {}'.format(valor1 + valor2))
+print('subtração {}'.format(valor1 - valor2))
+print('multiplicação {}'.format(valor1 * valor2))
+print('Divisão {}'.format(valor1 / valor2))
+print('potencia {}'.format(valor1 ** valor2))
+print('raiz quadrada {}'.format(valor1**(0.5)))
+print('raiz quadrada2 {}'.format(valor2**(0.5)))
