672bf7f5c61116f765c84aa5ffbe03ffd923a0c6
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2024-04-04 13:56:21 -0300

    cpmecei as aulas de python

---
+++ Logica de programação/AulasPython/aula01/TiposDados.py
@@ -0,0 +1,8 @@
+valor = 1
+nome = 'Gabriel'
+valorDecimal = 1.99
+
+print('tipos de dados, inteiro: {}, caractere: {}, valordecimal: {}'.format(valor, nome, valorDecimal))
+print(type(valor))
+print(type(valorDecimal))
+print(type(nome))
