e4608aa57987e67064d6ee631469280fd6742e1a
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2024-04-23 17:29:36 -0300

    aula

--- Logica de programação/AulasPython/Aula03/ex003.py
+++ Logica de programação/AulasPython/Aula03/ex003.py
@@ -1,4 +1,12 @@
 for c in range(11):
+    maior = 0
+    menor = 0
     numero = int(input('Informe um numero inteiro: '))
 
-    if 
+    if numero > maior:
+        maior = numero
+
+    elif numero < menor:
+        menor = numero
+
+print('O maior numer é {} e o menor numero é {}'.format(maior, menor))
