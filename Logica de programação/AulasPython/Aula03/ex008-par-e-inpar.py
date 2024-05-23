240ff9dae4fa621d8bf6365d273274cdefe60400
Gabriel Mazzuchello <94196035+GabrielMazzuchello@users.noreply.github.com>
2024-05-23 15:20:08 -0300

    ex008 consertado

--- Logica de programação/AulasPython/Aula03/ex008-par-e-inpar.py
+++ Logica de programação/AulasPython/Aula03/ex008-par-e-inpar.py
@@ -1,8 +1,8 @@
 while True:
     numero = int(input('Informe um valor: '))
-    if numero % 2 == 0:
-        print('O numero {} é par'.format(numero))
-    elif numero % 2 != 0:
-        print('O numero {} é inpar'format(numero))
-    elif numero == 0:
+    if  numero == 0:
         break
+    elif numero % 2 != 0:
+        print('O número {} é ímpar'.format(numero))
+    elif numero % 2 == 0:
+        print('O número {} é par'.format(numero))
