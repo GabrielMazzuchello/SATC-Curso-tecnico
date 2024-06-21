2f788318ac8716047faed058ddbd04a460d3d56d
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2024-06-21 00:15:52 -0300

    teste criada para comparar

--- Logica de programação/Projeto_forca/projeto_forca.py
+++ Logica de programação/Projeto_forca/projeto_forca.py
@@ -68,7 +68,7 @@ def desenhar_forca(erro):
     ]
     print(estagios[erro])
 
-palavras_secretas = ['python', 'programação', 'computador','algoritmo']
+palavras_secretas = ['python', 'programação', 'computador', 'algoritmo']
 letras_adivinhadas = []
 letras_corretas = []
 n_letras = []
@@ -83,57 +83,51 @@ for c in range(numero_letras):
     n_letras.append('_')
 
 while True:
-    print(" ".join(n_letras))
-    print(desenhar_forca(erro))
-    if vida != 0:
-
-        letra_a_verificar = input("Informe uma letra: ").lower().strip()
-        print('')
-
-        for c in palavra_escolhida:
-            if c in letra_a_verificar:
-                index = palavra_escolhida.index(c)
-                n_letras[index] = letra_a_verificar
-
-
-        if letra_a_verificar in palavra_escolhida:
-            letras_corretas.append(letra_a_verificar)
-            letras_adivinhadas.append(letra_a_verificar)
-            print('letra correta!')
-            print('')
-
-            print('Letras corretas: ')
-            print((" ".join(letras_corretas)))
-            print('')
-            print('Letras adivinhadas: ')
-            print((" ".join(letras_adivinhadas)))
-
-            print(" ".join(n_letras))
-
-            escolha = str(input('Você desejá tentar adivinhar a palavra? (S/N)')).upper().strip()
-            if escolha == 'S':
-                print('')
-                palavra = input('Informe a palavra: ').lower().strip()
-                print('')
-                if palavra == palavra_escolhida:
-                    print('Voce acertou parabens!!')
-                    break
-                else:
-                    print('Você errou! continue tentando')
-            else:
-                print('Então continue tentando adivinhar as letras!')
-                print('')
-        else:
-            letras_adivinhadas.append(letra_a_verificar)
-            vida -= 1
-            print('Letra errada!')
-            print('Suas vidas {}'.format(vida))
-            print('')
-            erro += 1
-    else:
-        print('Voce perdeu!!!')
+    print("Palavra: ", " ".join(n_letras))
+    desenhar_forca(erro)
+    
+    if vida == 0:
+        print('Você perdeu!!!')
         print('A palavra era {}'.format(palavra_escolhida))
         break
 
+    letra_a_verificar = input("Informe uma letra: ").lower().strip()
+    
+    if letra_a_verificar in letras_adivinhadas:
+        print('Você já tentou essa letra. Tente outra.')
+        continue
 
+    letras_adivinhadas.append(letra_a_verificar)
+    
+    if letra_a_verificar in palavra_escolhida:
+        print('Letra correta!\n')
+        for idx, letra in enumerate(palavra_escolhida):
+            if letra == letra_a_verificar:
+                n_letras[idx] = letra
+
+        if '_' not in n_letras:
+            print('Parabéns! Você adivinhou a palavra:', palavra_escolhida)
+            break
+
+        letras_corretas.append(letra_a_verificar)
         
+    else:
+        print('Letra errada!')
+        vida -= 1
+        erro += 1
+        print('Suas vidas: {}'.format(vida))
+    
+    print('Letras corretas: ', " ".join(letras_corretas))
+    print('Letras adivinhadas: ', " ".join(letras_adivinhadas))
+    print()
+    
+    escolha = input('Você deseja tentar adivinhar a palavra? (S/N) ').upper().strip()
+    if escolha == 'S':
+        palavra = input('Informe a palavra: ').lower().strip()
+        if palavra == palavra_escolhida:
+            print('Você acertou, parabéns!!')
+            break
+        else:
+            print('Você errou! Continue tentando.')
+
+print('Fim do jogo.')
