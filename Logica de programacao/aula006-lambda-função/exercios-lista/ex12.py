efedca4961f8c6403a575c270d5d91675cb74109
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2024-09-11 17:26:39 -0300

    fim da aula

--- Logica de programacao/aula006-lambda-função/exercios-lista/ex12.py
+++ Logica de programacao/aula006-lambda-função/exercios-lista/ex12.py
@@ -4,30 +4,23 @@ lista de preços e um valor de desconto percentual. A função deve usar map() e
 aplicar o desconto a cada item e retornar uma nova lista com os preços atualizados.
 Dica: Use uma função def para aplicar o desconto e utilize map() dentro dela."""
 
-"""def desconto():
-    if len(produtos) >= 5:
-        desconto = list(map(lambda x : x-(x*0.10)), produtos)
-    elif len(produtos) >= 10:
-        desconto = list(map(lambda x : x-(x*0.20)), produtos)
-    elif len(produtos) >= 15:
-        desconto = list(map(lambda x : x-(x*0.35)), produtos)
-    return desconto"""
-
+def aplica_desconto(produtos, percentual_desconto):
+    # Aplica o desconto a cada produto usando map e lambda
+    return list(map(lambda x: x - (x * percentual_desconto), produtos))
 
 produtos = []
+
 while True: 
     produto = float(input('Informe o valor do produto: '))
     produtos.append(produto)
 
-    if len(produtos) == 5:
-        print(list(map(lambda x : x-(x*0.35)), produtos))
-    elif len(produtos) == 10:
-        print(list(map(lambda x : x-(x*0.35)), produtos))
-    elif len(produtos) == 15:
-        print(list(map(lambda x : x-(x*0.35)), produtos))
-    
-
-    
-
-
-
+Verifica a quantidade de produtos e aplica o desconto correto
+    if len(produtos) >= 15:
+        # Desconto de 35% para 15 ou mais produtos
+        print(aplica_desconto(produtos, 0.35))
+    elif len(produtos) >= 10:
+        # Desconto de 20% para 10 ou mais produtos
+        print(aplica_desconto(produtos, 0.20))
+    elif len(produtos) >= 5:
+        # Desconto de 10% para 5 ou mais produtos
+        print(aplica_desconto(produtos, 0.10))
