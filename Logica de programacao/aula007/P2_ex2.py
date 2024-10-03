279858e52b7d861c83c36417daefcdab1c4dfd4e
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2024-10-03 11:00:47 -0300

    exercicios da prova

--- Logica de programacao/aula007/P2_ex2.py
+++ Logica de programacao/aula007/P2_ex2.py
@@ -1,43 +1,41 @@
-# aluno: Gabriel Mazzuchello Dal Molin Turma: 2137  Data: 18/09/2024
-# aluno: João Alejandro Brenis Borges
-
-funcionario = {}        # Cria um dicionário
+funcionario = {}
 salarioSoma = 0
 
-def addFuncionario():       #Função que adiciona funcionarios
-    nome = input('Informe o nome do funcionario: ')     #Variável que recebe o nome do funcionario
-    salario = float(input('Informe o salario do funcionario: ')) # recebe o salario do mesmo
-    salarioSoma += salario # faz a soma do salario para descobrir a media
-    funcionario.update({nome: salario}) # adiciona o funcionario com seu respectivo salario
+def addFuncionario():
+    global salarioSoma 
+    nome = input('Informe o nome do funcionario: ')
+    salario = float(input('Informe o salario do funcionario: '))
+    salarioSoma += salario
+    funcionario.update({nome: salario})
 
-quant_funcionario = int(input('Quantos funcionarios a empresa possui: ')) # pergunta quantos funcionarios a empresa tem
+quant_funcionario = int(input('Quantos funcionarios a empresa possui: '))
 
-for c in range(quant_funcionario):         #Percorre a quantidade de funcionarios digitados
-    addFuncionario() # chama a função addFuncionario
+for c in range(quant_funcionario):
+    addFuncionario()
 
-salarioFiltro = float(input('Informe o valor para filtrar os funcionarios: ')) # recebe o valor para filtrar os funcionarios
+salarioFiltro = float(input('Informe o valor para filtrar os funcionarios: '))
 
-funcionario_Filtrado = dict(filter(lambda x: x if x[1] > salarioFiltro else False, funcionario.items())) # filtra os funcionarios de acordo com o valor digitado
+funcionario_Filtrado = dict(filter(lambda x: x[1] > salarioFiltro, funcionario.items()))
 
-# funcionario_SalarioNovo = dict(map(lambda x : x[0] (x[1] * 1.20), funcionario_Filtrado.items())) Não funciona deveria acrecentar 20% ao salario do funcionario
+funcionario_SalarioNovo = {nome: salario * 1.20 for nome, salario in funcionario_Filtrado.items()}
 
-media_salarial = salarioSoma / quant_funcionario # caucula a media dos funcionarios
+media_salarial = salarioSoma / quant_funcionario
 
-print(f'A media salarial é {media_salarial}')
+print(f'A média salarial é {media_salarial}')
 
-count = 0 # contador para saber que essa é a primeira vez execultando o codigo para definir o maior e menor pela 1° vez
-for nome, valor in funcionario.items(): # percorre o dicionario funcionario
+count = 0 
+for nome, valor in funcionario.items():  
     if count == 0:
-        maior = valor  # todo o bloco até a linha 40 servem para verificar o maior e o menor valor
+        maior = valor
         menor = valor
-
     elif valor < menor:
         menor = valor
-
     elif valor > maior:
         maior = valor
-
     count += 1
 
-print(f'O funcionario com o maior salario é {maior} e o menor é {menor}')
+print(f'O funcionário com o maior salário ganha {maior} e o menor ganha {menor}')
 
+print('Funcionários com salário acima do filtro e com 20% de aumento:')
+for nome, salario in funcionario_SalarioNovo.items():
+    print(f'{nome}: {salario:.2f}')
