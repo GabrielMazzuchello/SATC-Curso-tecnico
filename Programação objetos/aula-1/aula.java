e9c613e4b5463d860c98ad703be380cc07efbcee
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2025-02-26 09:16:28 -0300

    configurações

--- Programação objetos/aula-1/aula.java
+++ Programação objetos/aula-1/aula.java
@@ -1,19 +1,20 @@
-import java.util.Scanner;  // Importação específica para Scanner
+import java.util.Scanner; // Importação específica para Scanner
 
 // A classe principal deve ser chamada "Main" conforme o enunciado
 class Main {
-    public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);  // Instanciação do Scanner
-        double Celcius, Farenheit;
 
-        System.out.print("Conversor de temperaturas");
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in); // Instanciação do Scanner
+    double Celcius, Farenheit;
 
-        System.out.print(" Digite a temperatura em Celcius: ");
-        Celcius = entrada.nextDouble();
+    System.out.print("Conversor de temperaturas");
 
-        Farenheit = (9 * Celcius + 160) / 5;
+    System.out.print(" Digite a temperatura em Celcius: ");
+    Celcius = entrada.nextDouble();
 
-        System.out.print("\n A medida convertida é " + Farenheit + "°F\n");
-        entrada.close();
-    }
+    Farenheit = (9 * Celcius + 160) / 5;
+
+    System.out.print("\n A medida convertida é " + Farenheit + "°F\n");
+    entrada.close();
+  }
 }
