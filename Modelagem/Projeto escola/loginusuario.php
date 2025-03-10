df0ddacae8c92100efff4ea247e8274c1c27f9cd
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2025-03-10 17:20:59 -0300

    criei o login fazer css

---
+++ Modelagem/Projeto escola/loginusuario.php
@@ -0,0 +1,42 @@
+<?php
+$acessar  = mysql_connect('localhost', 'root', '');
+$banco    = mysql_select_db('escola');
+
+if (isset($_POST['conectar'])) {
+    $login = $_POST['login'];
+    $senha = $_POST['senha'];
+
+    $sql = "SELECT login, senha FROM usuario
+        WHERE login = '$login' and senha = '$senha'";
+
+    $resultado = mysql_query($sql);
+
+    if (mysql_num_rows($resultado) == 0) {
+        echo "<script language='javascript' type='text/javascript'>
+        alert('Login e/ou senha incorretos');
+        window.location.href='loginusuario.php';
+        </script>";
+    } else {
+        setcookie('login', $login);
+        header('Location:menu.htm');
+    }
+}
+?>
+
+<HTML>
+
+<HEAD>
+    <TITLE>login usuario</TITLE>
+</HEAD>
+
+<BODY>
+    <form name="formulario" method="post" action="loginusuario.php">
+        Login: <input type="text" name="login" id="login" size=10>
+        <br><br>
+        Senha: <input type="password" name="senha" id="senha" size=10>
+        <br><br>
+        <input type="submit" name="conectar" value="conectar">
+    </form>
+</BODY>
+
+</HTML>
