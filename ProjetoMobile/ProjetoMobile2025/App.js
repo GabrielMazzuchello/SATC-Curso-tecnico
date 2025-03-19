d0707e82f535a733b22d32f64c7286f7dd90deb0
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2025-03-19 09:52:20 -0300

    tela home feita

--- ProjetoMobile/ProjetoMobile2025/App.js
+++ ProjetoMobile/ProjetoMobile2025/App.js
@@ -3,11 +3,12 @@ import Gallery, { Profile, Favorites } from "./src/components/Profile";
 import { StyleSheet } from "react-native";
 import { Teste } from "./src/components/teste";
 import { Login } from "./src/screens/login";
+import { Home } from "./src/screens/home";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Profile/>
+      <Home/>
     </View>
   );
 }
--- ProjetoMobile/ProjetoMobile2025/src/screens/home.js
+++ ProjetoMobile/ProjetoMobile2025/src/screens/home.js
@@ -1,69 +1,53 @@
-import {StyleSheet, Text, View, Image, ImageBackground} from "react-native"
-import imagemFundo from "../assets/netflix.png"
+import { StyleSheet, Text, View, Image } from "react-native";
 
-export function Home () {
-    return(
-        <View style={styles.home}>
-            <ImageBackground style={{flex: 1, widht: '100%', heigh: '100%'}} source={imagemFundo}>
-            </ImageBackground>
-        <View style={styles.text1}>
-            <Text style={styles.texts}>Zécaflix</Text> 
-        </View>
-        <View style={styles.imgs}>
-            <Image style={styles.img1} source={{uri:'https://m.media-amazon.com/images/I/91RzpxQktRL.jpg'}}/>
-            <Image style={styles.img1} source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKr78LlQAp91G2kspqG75p-EFtZ8r11hxEVg&s'}}/>
-        </View>
-        <View style={styles.text2}>
-            <Text style={styles.texts}>Assista já!</Text>
-        </View>
-        <View style={styles.imgs}>
-            <Image style={styles.img2} source={{uri:'https://m.media-amazon.com/images/M/MV5BMjU0NWMzOWMtZjM0MC00ZTE0LThmYzItZGQ1N2JmNDU1NGI4XkEyXkFqcGc@._V1_.jpg'}}/>
-            <Image style={styles.img2} source={{uri:'https://i.ebayimg.com/images/g/3TYAAOSwO0lkwEQu/s-l400.jpg'}}/>
-        </View>
-    </View>        
-);
+export function Home() {
+  return (
+    <View style={styles.profile}>
+      <Text>Este é a home!</Text>
+      <View style={styles.imgs}>
+        <Image style={styles.img} source={require("../../assets/elite.jpg")} />
+        <Image style={styles.img} source={require("../../assets/elite.jpg")} />
+      </View>
+
+      <View style={styles.textRight}>
+        <Text>Texto à direita</Text>
+      </View>
+
+      <View style={styles.imgs}>
+        <Image style={styles.img} source={require("../../assets/elite.jpg")} />
+        <Image style={styles.img} source={require("../../assets/elite.jpg")} />
+      </View>
+      <View style={styles.textCenter}>
+        <Text>Texto centralizado</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
-    flex: 1, 
-    alignItems: "center", 
-    justifyContent: "center", 
-    backgroundColor: "#20CFFF"
-},
-home: {
-    flex:1,
-    backgroundColor: '#000',
-    fontSize: 20,
-},
-img1 : {
-    width: 200,
-    height: 200,
-},
-img2 : {
-    width: 200,
-    height: 200,
-},
-text1: {
-    flex: 1
-},
-text2 : {
-    flex: 2,
-    alignSelf: 'flex-end',
-    marginRight: 20 
-},
-text3 : {
-    flex: 3,
-    alignSelf: 'center' 
-},
-imgs: {
-    justifyContent: 'space-evenly',
-    flexDirection: 'row'
-},
-texts: {
-    margin: 'auto', 
-    color: 'red',
-    fontSize: 30,
-    fontWeight: 'bold',
-}  
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#20CFFF",
+  },
+  profile: {
+    gap: 10,
+    flexDirection: "column",
+    flex: 1,
+    backgroundColor: "#F99",
+    padding: 10,
+  },
+  imgs: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  img: {
+    height: 175,
+    width: 175,
+  },
+  textRight: {
+    alignItems: "flex-end",
+  },
+  textCenter: {
+    alignItems: "center", // Centraliza verticalmente
+  },
+});
