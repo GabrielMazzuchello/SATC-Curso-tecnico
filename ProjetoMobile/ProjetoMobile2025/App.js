282d23f01ae34e99e5b26ba7fcace71dd78c4b04
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2025-03-26 14:45:04 -0300

    navegação de telas usando stack?

--- ProjetoMobile/ProjetoMobile2025/App.js
+++ ProjetoMobile/ProjetoMobile2025/App.js
@@ -1,23 +1,21 @@
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
 import { View } from "react-native";
-// Only import react-native-gesture-handler on native platforms
-import 'react-native-gesture-handler';
-import Gallery, { Profile, Favorites } from "./src/components/Profile";
-import { StyleSheet } from "react-native";
-import { Teste } from "./src/components/teste";
-import { Login } from "./src/screens/login";
+import { createStackNavigator } from "@react-navigation/stack";
 import { Home } from "./src/screens/home";
+import { Login } from "./src/screens/login";
+
+
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Home />
-      {/* <Login /> */}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
