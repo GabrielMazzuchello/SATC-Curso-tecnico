9f20409df8b138e1a7f64d2b968f994797b74d63
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2025-06-04 13:04:14 -0300

    exercicio java

---
+++ Programação objetos/exercicios/ex01_temperatura.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ex01_temperatura {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Informe os dias da semana");
+        int diaSemana = scanner.nextInt();
+        scanner.nextLine();
+
+        
+
+
+
+        scanner.close();
+    }
+
+
+    
+}
